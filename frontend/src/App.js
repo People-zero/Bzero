5e03a1fccb025fab79635d1833eba34ff3f447d6
hayoun <fishingest@kookmin.ac.kr>
2022-08-09 04:24:28 +0900

    [FEAT] Frontend 초기 세팅 끝

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+// import { BrowserRouter, Route, Routes } from 'react-router-dom';
 function App() {
   return (
+    // <BrowserRouter>
     <div className="App">
-      프론트엔드 화이팅~
+      프론트엔드팀 화이팅~
+      {/* <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/new' element={<New></New>}></Route>
+        <Route path='/edit/:id' element={<Edit></Edit>}></Route>
+        <Route path='/diary/:id' element={<Diary></Diary>}></Route>
+        
+      </Routes>
+       */}
     </div>
+    // </BrowserRouter>
   );
 }
 
-export default App;
+export default App
