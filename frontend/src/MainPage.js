514b426743c52f85a251ab8780af3942e75ec712
hayoun <fishingest@kookmin.ac.kr>
2022-08-14 03:08:21 +0900

    [FEAT] MainPage Secondpage
    메인페이지 두번째 페이지

--- frontend/src/MainPage.js
+++ frontend/src/MainPage.js
@@ -39,6 +39,37 @@ const MainPage=()=>{
  
     </div>
 
+    <div className="MainPage_secondPage">
+
+        <span className="MainPage_secondPage_Maintext">
+            제로웨이스트, 혹시 실천하는데 어려움이 있었나요? 
+        </span>
+        <span className="MainPage_secondPage_subtext">
+            제로웨이스트, 막상 마음은 먹었지만 어렵게 느껴지시나요?
+        </span>
+
+        <div className="MainPage_secondPage_blockblock">
+
+            <div className="MainPage_secondPage_block">
+                <span>뭐뭐가 어려워요! 뭐뭐한거 없을까요?
+                    <img src={process.env.PUBLIC_URL+`../img/Group 1148.png`}/> </span>
+            </div>
+            <div style={{backgroundColor:'#0679FF'}} className="MainPage_secondPage_block2">
+         <span><img src={process.env.PUBLIC_URL+`../img/Group 1148.png`}/>뭐뭐가 어려워요! 뭐뭐한거 없을까요?
+                     </span>
+            </div>
+            <div style={{backgroundColor:'#72EFCA'}}className="MainPage_secondPage_block">
+            <span>뭐뭐가 어려워요! 뭐뭐한거 없을까요?
+                    <img src={process.env.PUBLIC_URL+`../img/Group 1148.png`}/> </span>
+            </div>
+            <div style={{backgroundColor:'#8692FF'}} className="MainPage_secondPage_block2">
+            <span><img src={process.env.PUBLIC_URL+`../img/Group 1148.png`}/>뭐뭐가 어려워요! 뭐뭐한거 없을까요?
+                     </span>
+            </div>
+
+        </div>
+
+    </div>
 
     </div>
 )
