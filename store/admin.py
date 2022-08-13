369b7524aab9069e98da03b7cbcd728a9ab7e346
clark1015 <seok626898@gmail.com>
2022-08-13 15:33:39 +0900

    [FEAT] 공병 스토어, 클린 스토어 모델 분리, admin 추가

--- store/admin.py
+++ store/admin.py
@@ -2,9 +2,14 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-@admin.register(Store)
-class StoreAdmin(admin.ModelAdmin):
+@admin.register(Clean_Store)
+class Clean_Store_Admin(admin.ModelAdmin):
     pass
+
+@admin.register(Bottle_Collection_Store)
+class Bottle_Collection_Store_Admin(admin.ModelAdmin):
+    pass
+
 
 @admin.register(Review)
 class ReviewAdmin(admin.ModelAdmin):
--- store/serializers.py
+++ store/serializers.py
@@ -1,9 +1,9 @@
 from django.test import TestCase
 from rest_framework import serializers
-from .models import Store,Review
+from .models import Clean_Store,Review
 class TestStoreSerializer(serializers.ModelSerializer):
      class Meta:
-        model = Store
+        model = Clean_Store
         fields = ('id', 'store_name', 'opening_time', 'store_longtitude', 'store_latitude', 'telephone','description', 'point_avg')
 
 class TestReviewSerializer(serializers.ModelSerializer):
