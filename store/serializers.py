5fe438ebfdcc331ab347a07f2d07fc7c9d97f932
minseokey <lms990427@gmail.com>
2022-08-13 16:08:46 +0900

    [FEAT] viewset,serializer,router 기본 설정

--- store/serializers.py
+++ store/serializers.py
@@ -1,12 +1,28 @@
 from django.test import TestCase
 from rest_framework import serializers
 from .models import Clean_Store,Review
-class TestStoreSerializer(serializers.ModelSerializer):
-     class Meta:
-        model = Clean_Store
-        fields = ('id', 'store_name', 'opening_time', 'store_longtitude', 'store_latitude', 'telephone','description', 'point_avg')
 
-class TestReviewSerializer(serializers.ModelSerializer):
-     class Meta:
-        model = Review
-        fields = '__all__'
+
+class Clean_StoreSerializer(serializers.ModelSerializer):
+   pass
+
+class ReviewSerializer(serializers.ModelSerializer):
+   pass
+
+class Bottle_Collections_StoreSerializer(serializers.ModelSerializer):
+   pass
+
+
+
+
+
+
+# class TestStoreSerializer(serializers.ModelSerializer):
+#      class Meta:
+#         model = Clean_Store
+#         fields = ('id', 'store_name', 'opening_time', 'store_longtitude', 'store_latitude', 'telephone','description', 'point_avg')
+
+# class TestReviewSerializer(serializers.ModelSerializer):
+#      class Meta:
+#         model = Review
+#         fields = '__all__'
--- store/views.py
+++ store/views.py
@@ -2,55 +2,79 @@ from urllib import request, response
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from .models import Store,Review
-from .serializers import TestReviewSerializer,TestStoreSerializer
+from .models import Clean_Store,Review,Bottle_Collection_Store
+from . import serializers
 from django.shortcuts import get_object_or_404
 from rest_framework import status
+from rest_framework.viewsets import ModelViewSet
 
 # Create your views here.
-class TestStoreListAPIView(APIView):
-    def get(self,request):
-        qs = Store.objects.all()
-        serializer = TestStoreSerializer(qs,many = True)
-        return Response(serializer.data)
-    def post(selfrequest):
-        serializer = TestStoreSerializer(data = request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data,status = 201)
-        return Response(serializer.errors,status = 400)
+# class TestStoreListAPIView(APIView):
+#     def get(self,request):
+#         qs = Clean_Store.objects.all()
+#         serializer = TestStoreSerializer(qs,many = True)
+#         return Response(serializer.data)
+#     def post(selfrequest):
+#         serializer = TestStoreSerializer(data = request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data,status = 201)
+#         return Response(serializer.errors,status = 400)
 
-class TestStoreDetailAPIView(APIView):
-    def get_object(self,pk):
-        return Store.objects.get(pk=pk)
+# class TestStoreDetailAPIView(APIView):
+#     def get_object(self,pk):
+#         return Clean_Store.objects.get(pk=pk)
 
-    def get(self, request, pk):
-        store = self.get_object(pk)
-        serializer = TestStoreSerializer(store)
-        return Response(serializer.data)
+#     def get(self, request, pk):
+#         store = self.get_object(pk)
+#         serializer = TestStoreSerializer(store)
+#         return Response(serializer.data)
     
-    def put(self, request, pk):
-        store = self.get_object(pk)
-        serializer = TestStoreSerializer(store, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#     def put(self, request, pk):
+#         store = self.get_object(pk)
+#         serializer = TestStoreSerializer(store, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def delete(self, request, pk):
-        store = self.get_object(pk)
-        store.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+#     def delete(self, request, pk):
+#         store = self.get_object(pk)
+#         store.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-class TestReviewAPIView(APIView):
-    def get(self,request):
-        qs = Review.objects.all()
-        serializer = TestReviewSerializer(qs,many = True)
-        return Response(serializer.data)
-    def post(self,request):
-        serializer = TestReviewSerializer(data = request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data,status = 201)
-        return Response(serializer.errors,status = 400)
+# class TestReviewAPIView(APIView):
+#     def get(self,request):
+#         qs = Review.objects.all()
+#         serializer = TestReviewSerializer(qs,many = True)
+#         return Response(serializer.data)
+#     def post(self,request):
+#         serializer = TestReviewSerializer(data = request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data,status = 201)
+#         return Response(serializer.errors,status = 400)
+
+class Clean_StoreViewSet(ModelViewSet):
+    quereyset = (
+        Clean_Store.objects.all()
+    )
+    serializer_class = serializers.Clean_StoreSerializer
+
+class ReviewViewSet(ModelViewSet):
+    queryset = (
+        Review.objects.all()
+    )
+    serializer_class = serializers.ReviewSerializer
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        qs = qs.filter(Clean_Store__pk=self.kwargs["Clean_Store_pk"])
+        return qs
+
+class Bottle_Collection_StoreViewSet(ModelViewSet):
+    queryset = (
+        Bottle_Collection_Store.objects.all()
+    )
+    serializer_class = serializers.Bottle_Collection_StoreSerializer
