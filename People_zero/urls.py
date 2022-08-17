80a42ffecdedce977e928be13da1fed62fe6123e
yeonsu <jack0810@kookmin.ac.kr>
2022-08-17 14:32:15 +0900

    [FIX] fix PeopleZero.url

--- People_zero/urls.py
+++ People_zero/urls.py
@@ -12,5 +12,5 @@ urlpatterns = [
     path('', include('backend.urls')),
     path('frontend/', TemplateView.as_view(template_name='index.html')),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('test1/', include('accounts.urls'))
+    path('auth/', include('accounts.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
