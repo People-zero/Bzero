22268cb0ff46cf5949a0b510f726fd3be1cce6a7
Eun-sun-Lee <eunsun2080@kookmin.ac.kr>
2022-08-19 22:55:27 +0900

    [style] complete CleanStoreDetial Page top_bar

--- frontend/src/CleanStoreDetail.js
+++ frontend/src/CleanStoreDetail.js
@@ -1,9 +1,47 @@
+import SideBar from "./components/SideBar";
+import { useState,useEffect,useContext } from "react";
+import { useNavigate,useParams } from "react-router";
+import { CleanStoreContext } from "./App";
+
+const env=process.env;
+env.PUBLIC_URL = env.PUBLIC_URL || "";
+
 const CleanStoreDetail = () => {
+
+    const {id} = useParams();
+    const cleanStoreList = useContext(CleanStoreContext);
+    const navigate = useNavigate();
+    const [data,setData] = useState();
+
+    useEffect(()=>{
+        if (cleanStoreList.length >=1){
+            const targetList = cleanStoreList.find((it)=>parseInt(it.id)===parseInt(id));
+
+            if(targetList){ //가게가 존재할 때
+                setData(targetList);
+            } else{ //가게가 없을 때
+                alert("없는 가게입니다.");
+                navigate("/cleanstore",{replace:true}); //뒤로가기 막음.
+            }
+        }
+    },[id,cleanStoreList]);
+
+    if(!data) {
+        return <div className="CleanStoreDetail">로딩중입니다...</div>;
+    }else{
     return (
-        <div>
-            <h2>클린스토어 상세정보</h2>
+        <div className="CleanStoreDetail">
+            <section className="menu_wrapper"><SideBar /></section>
+            <div className="cleanstore_detail">
+            <section className="cleanstore_topbar">
+                <h2>클린스토어 상세정보</h2></section>
+            <section className="cleanstore_content">
+            <h2>{data.name}</h2>
+            </section>
+            </div>
         </div>
     );
+};
 };
 
 export default CleanStoreDetail;
