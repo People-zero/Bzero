2b83feabe1a6a665686a366ba071750928f795f9
hayoun <fishingest@kookmin.ac.kr>
2022-08-21 03:20:01 +0900

    [FEAT] Kakao map api service setting
    카카오 맵 api 서비스 다운 세팅

--- frontend/src/MapPage.js
+++ frontend/src/MapPage.js
@@ -13,7 +13,7 @@ const MapPage = () => {
 		        level: 5, // 지도의 확대 레벨
 		        mapTypeId : kakao.maps.MapTypeId.ROADMAP // 지도종류
 		    }; 
-
+var infowindow = new kakao.maps.InfoWindow({zIndex:1});
 		// 지도를 생성한다 
 		var map = new kakao.maps.Map(mapContainer, mapOption); 
 
@@ -25,6 +25,8 @@ const MapPage = () => {
 
 		// 지도에 확대 축소 컨트롤을 생성한다
 		var zoomControl = new kakao.maps.ZoomControl();
+
+    
 
 		// 지도의 우측에 확대 축소 컨트롤을 추가한다
 		map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
@@ -57,6 +59,46 @@ const MapPage = () => {
 		kakao.maps.event.addListener(marker, 'click', function() {
 		    alert('마커를 클릭했습니다!');
 		});
+        var ps = new kakao.maps.services.Places(); 
+
+// 키워드로 장소를 검색합니다
+
+// 키워드 검색 완료 시 호출되는 콜백함수 입니다
+function placesSearchCB (data, status, pagination) {
+    if (status === kakao.maps.services.Status.OK) {
+
+        // 검색된 장소 위치를 기준으로 지도 범위를 재설정하기위해
+        // LatLngBounds 객체에 좌표를 추가합니다
+        var bounds = new kakao.maps.LatLngBounds();
+
+        for (var i=0; i<data.length; i++) {
+            displayMarker(data[i]);    
+            bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x));
+        }       
+
+        // 검색된 장소 위치를 기준으로 지도 범위를 재설정합니다
+        map.setBounds(bounds);
+    } 
+}
+ps.keywordSearch('국민대학교', placesSearchCB); 
+
+// 지도에 마커를 표시하는 함수입니다
+function displayMarker(place) {
+    
+    // 마커를 생성하고 지도에 표시합니다
+    var marker = new kakao.maps.Marker({
+        map: map,
+        position: new kakao.maps.LatLng(place.y, place.x) 
+    });
+
+    // 마커에 클릭이벤트를 등록합니다
+    kakao.maps.event.addListener(marker, 'click', function() {
+        // 마커를 클릭하면 장소명이 인포윈도우에 표출됩니다
+        infowindow.setContent('<div>' + place.place_name + '</div>');
+        infowindow.open(map, marker);
+    });
+}
+
   }, []);
 
   return (
