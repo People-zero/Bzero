7aaa40e6016697d8a1f97997cd545f4dfbbbe21f
hayoun <fishingest@kookmin.ac.kr>
2022-08-22 02:18:52 +0900

    [FEAT]Kakao Map Search and Markers
    카카오 맵 검색 기능, 마커 기능 추가

--- frontend/src/MapPage.js
+++ frontend/src/MapPage.js
@@ -1,11 +1,16 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 import "./css/MapPage.css";
 
 import "./css/Mapnav.css";
+import MapNav from "./components/MapNav";
+
+
 const { kakao } = window;
 
 const MapPage = () => {
+    
+    const [where,setwhere]=useState("정릉시장")
   useEffect(() => {
     var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
 		    mapOption = {
@@ -31,8 +36,10 @@ var infowindow = new kakao.maps.InfoWindow({zIndex:1});
 		// 지도의 우측에 확대 축소 컨트롤을 추가한다
 		map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
 		// 마커 이미지의 주소
-		var markerImageUrl = 'https://t1.daumcdn.net/localimg/localimages/07/2012/img/marker_p.png', 
-		    markerImageSize = new kakao.maps.Size(40, 42), // 마커 이미지의 크기
+		var markerImageUrl = '../img/Group 1182 (1).png', 
+        // 'https://t1.daumcdn.net/localimg/localimages/07/2012/img/marker_p.png',
+        // '../img/Group 1182 (1).png', 
+		    markerImageSize = new kakao.maps.Size(36, 42), // 마커 이미지의 크기
 		    markerImageOptions = { 
 		        offset : new kakao.maps.Point(20, 42)// 마커 좌표에 일치시킬 이미지 안의 좌표
 		    };
@@ -40,25 +47,47 @@ var infowindow = new kakao.maps.InfoWindow({zIndex:1});
 		// 마커 이미지를 생성한다
 		var markerImage = new kakao.maps.MarkerImage(markerImageUrl, markerImageSize, markerImageOptions);
 
+        const place=[{
+            place_name:"하윤집",
+            x:37.6084606,
+            y:127.0094845,
+        },
+    {
+        place_name:"축산",
+            x:37.6054606,
+            y:127.0094845,
+    },{place_name:"test",
+            x:37.6064606,
+            y:127.0094845,
+    },{
+        place_name:"하윤집",
+        x:37.6084606,
+        y:127.0074845,
+    },
+]
+
+        for(var i=0;i<place.length;i++){
 		// 지도에 마커를 생성하고 표시한다
 		var marker = new kakao.maps.Marker({
-		    position: new kakao.maps.LatLng(37.56670, 126.97522), // 마커의 좌표
+		    position: new kakao.maps.LatLng(place[i].x, place[i].y), // 마커의 좌표
 		    image : markerImage, // 마커의 이미지
 		    map: map // 마커를 표시할 지도 객체
 		});
 
 		// 마커 위에 표시할 인포윈도우를 생성한다
 		var infowindow = new kakao.maps.InfoWindow({
-		    content : '<div style="padding:5px;">인포윈도우 :D</div>' // 인포윈도우에 표시할 내용
+		    content : '<div class="infovar">공병 수거가 가능합니다!</div>' // 인포윈도우에 표시할 내용
 		});
 
 		// 인포윈도우를 지도에 표시한다
-		infowindow.open(map, marker);
+		// infowindow.open(map, marker);
 
 		// 마커에 클릭 이벤트를 등록한다 (우클릭 : rightclick)
 		kakao.maps.event.addListener(marker, 'click', function() {
 		    alert('마커를 클릭했습니다!');
 		});
+    }
+
         var ps = new kakao.maps.services.Places(); 
 
 // 키워드로 장소를 검색합니다
@@ -72,7 +101,7 @@ function placesSearchCB (data, status, pagination) {
         var bounds = new kakao.maps.LatLngBounds();
 
         for (var i=0; i<data.length; i++) {
-            displayMarker(data[i]);    
+            // displayMarker(data[i]);    
             bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x));
         }       
 
@@ -80,48 +109,38 @@ function placesSearchCB (data, status, pagination) {
         map.setBounds(bounds);
     } 
 }
-ps.keywordSearch('국민대학교', placesSearchCB); 
+ps.keywordSearch(where, placesSearchCB);
+
 
 // 지도에 마커를 표시하는 함수입니다
-function displayMarker(place) {
+// function displayMarker(place) {
     
-    // 마커를 생성하고 지도에 표시합니다
-    var marker = new kakao.maps.Marker({
-        map: map,
-        position: new kakao.maps.LatLng(place.y, place.x) 
-    });
+//     // 마커를 생성하고 지도에 표시합니다
+//     var marker = new kakao.maps.Marker({
+//         map: map,
+//         position: new kakao.maps.LatLng(place.y, place.x) 
+//     });
 
-    // 마커에 클릭이벤트를 등록합니다
-    kakao.maps.event.addListener(marker, 'click', function() {
-        // 마커를 클릭하면 장소명이 인포윈도우에 표출됩니다
-        infowindow.setContent('<div>' + place.place_name + '</div>');
-        infowindow.open(map, marker);
-    });
-}
+//     // 마커에 클릭이벤트를 등록합니다
+//     kakao.maps.event.addListener(marker, 'click', function() {
+//         // 마커를 클릭하면 장소명이 인포윈도우에 표출됩니다
+//         infowindow.setContent('<div>' + place.place_name + '</div>');
+//         infowindow.open(map, marker);
+//     });
+// }
 
-  }, []);
+// var Marker=displayMarker(place)
 
+
+  }, [where]);
+  
+  const [searchwhere,setsearchwhere]=useState(where)
   return (
     <div className="Map">
-    <div className="Map_nav">
-
-    <div className="Map_nav_image"><img src={process.env.PUBLIC_URL+`../img/로고 파랑 2.png`}></img> BZero</div>
-    
-    <div className="Map_nav_sellect">
-        <a href="/mypage"><div className="Map_nav_subtext">마이페이지</div></a>
-        <div className="Map_nav_subtext" style={{color:'#0679FF'}}>가게 찾기</div>
-        <div className="Map_nav_subsub">
-            <div>클린스토어</div>
-            <div style={{color:'#353535'}}>공병스토어</div>
+        <MapNav></MapNav>
+        <div className="MapPage_search">
+            <input placeholder="지역을 검색해주세요!" value={searchwhere} onChange={(e)=>setsearchwhere(e.target.value)}></input><img onClick={()=>setwhere(searchwhere) } src={process.env.PUBLIC_URL+`../img/Group 1140.png`}></img>
         </div>
-        <div className="Map_nav_subtext">커뮤니티</div>
-        <div className="Map_nav_subtext">제로웨이스트 일기</div>
-        
-    </div>
-
-    
-
-    </div>
       <div className="MapContainer" id="map">
       </div>
     </div>
