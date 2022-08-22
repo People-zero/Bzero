b3e618459646a778c31359f3aa403a67b2dba94a
yuujinkim <66306573+yuujinkim@users.noreply.github.com>
2022-08-23 06:30:29 +0900

    feat: filtering (ordering, search)

--- board/views.py
+++ board/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
-from .serializer import CommentSerializer, PostSerializer
+from .serializer import PostSerializer
 from . import models
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
 
 
 class PostViewSet(ModelViewSet):
     queryset = models.Post.objects.filter(is_public=True)
     serializer_class = PostSerializer
+
+    filter_backends = [DjangoFilterBackend, OrderingFilter, SearchFilter]
+    filterset_fields = []
+    ordering_fields = ['updated_at', 'recommend_cnt']
+    ordering = ['-updated_at']
+    search_fields = ['user', 'title', 'content']
 
 
 # class CommentViewSet(ModelViewSet):
