c869533ba7e3fd7a10f595e8da5311154f4e37bd
yuujinkim <66306573+yuujinkim@users.noreply.github.com>
2022-08-23 06:09:26 +0900

    feat: board models 수정

--- board/models.py
+++ board/models.py
@@ -1,26 +1,30 @@
 from django.db import models
-# from accounts.models import User
+from accounts.models import User
 
 
 class Post(models.Model):
-    # user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     title = models.CharField(max_length=30)
     content = models.TextField()
     image = models.ImageField(null=True, blank=True)
-    created_at = models.DateTimeField()
-    updated_at = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     recommend_cnt = models.IntegerField(default=0)
     is_public = models.BooleanField()
+    tag_set = models.ManyToManyField('Tag', blank=True)
 
 
 class Tag(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    tag_name = models.CharField(max_length=10)
+    tag_name = models.CharField(max_length=10, unique=True)
+
+    def __str__(self):
+        return self.tag_name
 
 
 class Comment(models.Model):
-    # user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    created_at = models.DateTimeField()
-    updated_at = models.DateTimeField()
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    post = models.ForeignKey(
+        Post, related_name='comments', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     content = models.TextField()
