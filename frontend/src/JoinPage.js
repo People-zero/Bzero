7ea8948bf52b966fd14fcf3d14eb1d5da1b43936
hayoun <fishingest@kookmin.ac.kr>
2022-09-01 15:55:32 +0900

    [FIX] Components

--- frontend/src/JoinPage.js
+++ frontend/src/JoinPage.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import LoginPageHeader from "./component/LoginPageHeader";
+import LoginPageHeader from "./components/LoginPageHeader";
 import "./css/JoinPage.css";
 const JoinPage = () => {
   const [isStaff, setIsStaff] = useState(false);
