580df6d4c6d24e593956ec028c06594d2259883d
yeonsu <jack0810@kookmin.ac.kr>
2022-09-01 19:04:41 +0900

    [FEAT] append setting

--- People_zero/settings.py
+++ People_zero/settings.py
@@ -162,6 +162,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
     ]
 }
 
