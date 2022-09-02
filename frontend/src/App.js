0fc7386fda47956d4274c608059583599611e171
suwith <suwith@kookmin.ac.kr>
2022-09-02 16:52:36 +0900

    [design] BottleStorePage css  수정

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,22 @@
-//import React, {useState, useEffect} from "react";
+import React, {useState, useEffect} from "react";
 import './App.css';
 import BottleStore from './BottleStore';
-//import RegistStore from "./RegistStore";
-import info_photo from "./images/info_photo.jpg";
+import RegistStore from "./RegistStore";
+import info_photo from "./images/info_photo.png";
 import photo1 from "./images/photo1.png";
 import photo2 from "./images/photo2.png";
 import photo3 from "./images/photo3.png";
-//import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import React,{useEffect, useReducer,useRef, useState} from 'react';
-import axios from 'axios';
-import RegistStore from './RegistStore';
-//import { dummyData } from './util/dummyData';
-//import { dummyReview } from './util/dummyReview';
-//import Point from './components/Point';
 
 const dummyList = [
   {
     name : "공병스토어1",
-    store_url:"www.naver.com",
     grade : 3,
-    info_summary : "이곳은 어쩌구저쩌구... 지구를 지키자...이곳은 어쩌구저쩌구... 지구를 지키자...이곳은 어쩌구저쩌구... 지구를 지키자...이곳은 어쩌구저쩌구... 지구를 지키자...이곳은 어쩌구저쩌구... 지구를 지키자...이곳은 어쩌구저쩌구... 지구를 지키자...이곳은 어쩌구저쩌구... 지구를 지키자...이곳은 어쩌구저쩌구... 지구를 지키자...",
-    photo: info_photo,
-    photo1:photo1,
-    photo2:photo2,
-    photo3:photo3,
+    info_summary : "이곳은 어쩌구저쩌구... 지구를 지키자...이곳은 어쩌구저쩌구... 지구를 지키자...이곳은 어쩌구저쩌구... 지구를 지키자...이곳은 어쩌구저쩌구... 지구를 지키자...이곳은 어쩌구저쩌구... 지구를 지키자...이곳은 어쩌구저쩌구... 지구를 지키자...이곳은 어쩌구저쩌구... 지구를 지키자...이곳은 어쩌구저쩌구... 지구를 지키자...이곳은 어쩌구저쩌구... 지구를 지키자...이곳은 어쩌구저쩌구... 지구를 지키자...이곳은 어쩌구저쩌구... 지구를 지키자...이곳은 어쩌구저쩌구... 지구를 지키자...이곳은 어쩌구저쩌구... 지구를 지키자...",
+    info_photo: info_photo,
+    photo1: photo1,
+    photo2: photo2,
+    photo3: photo3,
+    store_url:"www.naver.com",
     map: "",
     businessDay : "평일",
     businessHour : "오전 10시 - 오후 7시",
@@ -31,24 +24,23 @@ const dummyList = [
     CollectDay : "매주 수요일",
     CollectHour :"오전 10시 - 오후 7시",
     address : "서울특별시 OO구 OO 길 1232-12(OO동)",
-    inquiries : "02-OOO-OOOO",
+    number : "02-OOO-OOOO",
     info_title:"가게에 대한 설명 (부제목)",
-    info:"가게에 대한 설명~가게에 대한 설명~가게에 대한 설명~가게에 대한 설명~가게에 대한 설명~가게에 대한 설명~가게에 대한 설명~가게에 대한 설명~가게에 대한 설명~가게에 대한 설명~가게에 대한 설명~가게에 대한 설명~가게에 대한 설명~가게에 대한 설명~가게에 대한 설명~가게에 대한 설명~가게에 대한 설명~가게에 대한 설명~가게에 대한 설명~가게에 대한 설명~가게에 대한 설명~가게에 대한 설명~가게에 대한 설명~가게에 대한 설명~가게에 대한 설명~가게에 대한 설명~가게에 대한 설명~가게에 대한 설명~가게에 대한 설명~가게에 대한 설명~가게에 대한 설명~가게에 대한 설명~가게에 대한 설명~가게에 대한 설명~가게에 대한 설명~가게에 대한 설명~가게에 대한 설명~가게에 대한 설명~가게에 대한 설명~가게에 대한 설명~가게에 대한 설명~가게에 대한 설명~가게에 대한 설명~가게에 대한 설명~가게에 대한 설명~가게에 대한 설명~가게에 대한 설명~가게에 대한 설명~가게에 대한 설명~가게에 대한 설명~",
+    info:"가게에 대한 설명~가게에 대한 설명~가게에 대한 설명~가게에 대한 설명~가게에 대한 설명~가게에 대한 설명~가게에 대한 설명~가게에 대한 설명~가게에 대한 설명~가게에 대한 설명~가게에 대한 설명~가게에 대한 설명~가게에 대한 설명~가게에 대한 설명~가게에 대한 설명~가게에 대한 설명~가게에 대한 설명~가게에 대한 설명~가게에 대한 설명~가게에 대한 설명~가게에 대한 설명~가게에 대한 설명~가게에 대한 설명~가게에 대한 설명~가게에 대한 설명~가게에 대한 설명~가게에 대한 설명~가게에 대한 설명~가게에 대한 설명~가게에 대한 설명~가게에 대한 설명~가게에 대한 설명~가게에 대한 설명~가게에 대한 설명~가게에 대한 설명~가게에 대한 설명~가게에 대한 설명~가게에 대한 설명~가게에 대한 설명~가게에 대한 설명~가게에 대한 설명~가게에 대한 설명~가게에 대한 설명~가게에 대한 설명~가게에 대한 설명~가게에 대한 설명~가게에 대한 설명~가게에 대한 설명~가게에 대한 설명~가게에 대한 설명~가게에 대한 설명~가게에 대한 설명~가게에 대한 설명~가게에 대한 설명~가게에 대한 설명~가게에 대한 설명~",
 
   },
 ]
 
-
-
 function App() {
 
   return (
-    <div className='App'>
-      <BottleStore store={dummyList}/>
+    <div className="App">
       {/*<RegistStore/>*/}
+      < BottleStore store={dummyList}/>
     </div>
-
   );
 }
 
 export default App;
+
+// < BottleStore store={dummyList}/>
