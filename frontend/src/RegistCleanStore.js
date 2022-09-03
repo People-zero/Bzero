3431cf77fcfc6dff5c0723ba68c8e9df71f628e6
suwith <suwith@kookmin.ac.kr>
2022-09-03 18:18:05 +0900

    [rename] rename filename/classname

--- frontend/src/RegistCleanStore.js
+++ frontend/src/RegistCleanStore.js
@@ -1,4 +1,4 @@
-import './RegistStore.css';
+import './RegistCleanStore.css';
 import { useState,useRef, useContext } from 'react';
 import { useNavigate } from "react-router-dom";
 import icon_earth from './images/icon1.svg'
@@ -8,7 +8,7 @@ import mainlogo from './images/logo2.svg'
 const env = process.env;
 env.PUBLIC_URL = env.PUBLIC_URL || "";
 
-const RegistStore = ({store})=>{
+const registcleanstore = ({store})=>{
 
   const contentRef = useRef();
   //const navigate = useNavigate();
@@ -44,97 +44,97 @@ const handleSubmit = () => {
 }
 
   return (
-    <div className="registstore">
-      <div className="registstore_navbar_left">
-        <div className='registstore_navbar_left_title'>
+    <div className="registcleanstore">
+      <div className="registcleanstore_navbar_left">
+        <div className='registcleanstore_navbar_left_title'>
           <p><img src={mainlogo} width='130px'/></p>
         </div>
-        <div className='registstore_navbar_left_menu'>
+        <div className='registcleanstore_navbar_left_menu'>
           <a href="#">마이페이지</a>
           <a href="#">가게 찾기</a>
-          <a href="#" className='registstore_present'>가게 등록</a>
+          <a href="#" className='registcleanstore_present'>가게 등록</a>
           <a href="#">커뮤니티</a>
           <a href="#">제로웨이스트 일기</a>
         </div>
-        <div className='registstore_navbar_left_logout'>
+        <div className='registcleanstore_navbar_left_logout'>
           <a href='#'>로그아웃</a>
         </div>
       </div>
-      <div className='registstore_right'>
-        <div className='registstore_navbar_top'>
-          <div className='registstore_text'>
-            <div className='registstore_line1'>
-              <p className='registstore_title1'>스토어 등록</p>
-              <p className='registstore_title2'>클린 스토어</p>
+      <div className='registcleanstore_right'>
+        <div className='registcleanstore_navbar_top'>
+          <div className='registcleanstore_text'>
+            <div className='registcleanstore_line1'>
+              <p className='registcleanstore_title1'>스토어 등록</p>
+              <p className='registcleanstore_title2'>클린 스토어</p>
             </div>
             <p>아래 내용들을 입력해서 스토어를 등록해보세요</p>
           </div>
-          <div className='registstore_logo'>
+          <div className='registcleanstore_logo'>
             <img src={icon_earth} width='70px'/>
           </div>
         </div>
-        <div className='registstore_main'>
-          <div className='registstore_boxes'>
-            <div className='registstore_box_question'>
-              <div className='registstore_box'>
-                <div className='registstore_Q'>가게 이름을 작성해주세요</div>
+        <div className='registcleanstore_main'>
+          <div className='registcleanstore_boxes'>
+            <div className='registcleanstore_box_question'>
+              <div className='registcleanstore_box'>
+                <div className='registcleanstore_Q'>가게 이름을 작성해주세요</div>
                 <textarea 
                 name='name'
                 value={state.name}
-                className='registstore_A' 
+                className='registcleanstore_A' 
                 onChange = {handleChangeState}
                 placeholder='내용을 입력해주세요'/>
               </div>
-              <div className='registstore_box'>
-                <div className='registstore_Q'>가게 정보를 작성해주세요</div>
+              <div className='registcleanstore_box'>
+                <div className='registcleanstore_Q'>가게 정보를 작성해주세요</div>
                 <textarea 
                 name='info_summamary'
-                className='registstore_A'
+                className='registcleanstore_A'
                 value = {state.info_summary} 
                 onChange = {handleChangeState}
                 placeholder='내용을 입력해주세요'/>
               </div>
-              <div className='registstore_box'>
-                <div className='registstore_Q'>어떤 가게인지 자세한 설명을 작성해주세요</div>
-                <textarea className='registstore_A' 
+              <div className='registcleanstore_box'>
+                <div className='registcleanstore_Q'>어떤 가게인지 자세한 설명을 작성해주세요</div>
+                <textarea className='registcleanstore_A' 
                 name='info'
                 value = {state.info} 
                 onChange = {handleChangeState}
                 placeholder='내용을 입력해주세요'/>
               </div>
-              <div className='registstore_box'>
-                <div className='registstore_Q'>가게 주소를 작성해주세요</div>
+              <div className='registcleanstore_box'>
+                <div className='registcleanstore_Q'>가게 주소를 작성해주세요</div>
                 <textarea 
                 name='address'
-                className='registstore_A'
+                className='registcleanstore_A'
                 value = {state.address} 
                 onChange = {handleChangeState}
                 placeholder='내용을 입력해주세요'/>
               </div>
-              <div className='registstore_box'>
-                <div className='registstore_Q'>가게 연락처를 작성해주세요</div>
+              <div className='registcleanstore_box'>
+                <div className='registcleanstore_Q'>가게 연락처를 작성해주세요</div>
                 <textarea 
-                className='registstore_A' 
+                className='registcleanstore_A' 
                 name='number'
                 value = {state.number} 
                 onChange = {handleChangeState}
                 placeholder='내용을 입력해주세요'/>
               </div>
-              <div className='registstore_box'>
-                <div className='registstore_Q'>관련 링크가 있다면 첨부해주세요</div>
+              <div className='registcleanstore_box'>
+                <div className='registcleanstore_Q'>관련 링크가 있다면 첨부해주세요</div>
                 <textarea 
                 name='streo_url'
                 value={state.store_url}
-                className='registstore_A' 
+                className='registcleanstore_A' 
                 onChange = {handleChangeState}
                 placeholder='내용을 입력해주세요'/>
               </div>
             </div>
-            <div className='registstore_box_img'>
-                <div className='registstore_order'>사진을 첨부해주세요</div>
-                <div className='registstore_input_img'>
+            <div className='registcleanstore_box_img'>
+                <div className='registcleanstore_order'>사진을 첨부해주세요</div>
+                <div className='registcleanstore_input_img'>
                   <label for="input_file" 
-                  className="registstore_input_file">
+                  className="registcleanstore_input_file">
                   <img src={photo_icon} />
                   {/*사진미리보기 기능추가*/}
                   </label>
@@ -142,12 +142,12 @@ const handleSubmit = () => {
                 </div>
             </div>
           </div>
-          <div className='registstore_main_bottom'>
-            <a className='registstore_back' 
+          <div className='registcleanstore_main_bottom'>
+            <a className='registcleanstore_back' 
             href='#'
             /*onClick={() => navigate(-1)}*/>← 작성 취소</a>
             <button 
-            className='registstore_save' 
+            className='registcleanstore_save' 
             onClink={handleSubmit}>작성하기</button>
           </div>
         </div>
@@ -156,4 +156,4 @@ const handleSubmit = () => {
   );
 };
 
-export default RegistStore;
+export default RegistCleanStore;
