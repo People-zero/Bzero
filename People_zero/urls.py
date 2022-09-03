5033a7883f7da9f2619e99ec4b4ce546fe7e5d42
hayoun <fishingest@kookmin.ac.kr>
2022-09-03 16:37:25 +0900

    [Merge]Fix error

--- People_zero/urls.py
+++ People_zero/urls.py
@@ -17,7 +17,7 @@ urlpatterns = [
     path('anpi-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('post/',include('board.urls')),
     path('store/', include('store.urls')),
-    path('auth/', include('accounts.urls'))
+    path('auth/', include('accounts.urls')),
 
     path('frontend/', TemplateView.as_view(template_name='index.html')),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
