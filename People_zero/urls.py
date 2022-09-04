200c68b5fc916970e5fb34e1b4a6e8b25ab2eac7
minseokey <lms990427@gmail.com>
2022-09-04 19:43:09 +0900

    switch branch

--- People_zero/urls.py
+++ People_zero/urls.py
@@ -13,15 +13,9 @@ urlpatterns = [
     path('', include('backend.urls')),
     path('frontend/', TemplateView.as_view(template_name='index.html')),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-<<<<<<< HEAD
     path('post/',include('board.urls')),
     path('store/', include('store.urls')),
     path('auth/', include('accounts.urls'))
 
-=======
-    path('test1/', include('accounts.urls')),
-    path('storetest/', include('store.urls')),
-    path('post/',include('board.urls')),
->>>>>>> f659e16b91ac0931475f62735bacd6f8757f81d5
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
