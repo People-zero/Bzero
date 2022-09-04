988de3da7d2806f3e326df9166fcbba723e84a38
AnCha <tory912@gmail.com>
2022-09-04 23:26:41 +0900

    [FEAT] 작업중

--- frontend/src/DiaryDetailPage.js
+++ frontend/src/DiaryDetailPage.js
@@ -13,40 +13,42 @@ const DiaryDetailPage = ({ dummy_diary }) => {
           <p className="diary_detail_title">제로웨이스트 일기</p>
         </div>
         <div className="diary_detail_body">
-          {dummy_diary
-            .filter((x) => x.date === date)
-            .map((it) => (
-              <div>
-                <div className="diary_detail_top">
-                  <div className="diary_detail_date">
-                    <p className="diary_detail_post_date">{it.date}</p>
-                  </div>
-                  <div className="diary_detail_post_title">{it.title}</div>
-                  <div className="diary_detail_diary_info">
-                    <div className="diary_detail_author_profile">
-                      <img
-                        src={process.env.PUBLIC_URL + `/img/${it.profile}`}
-                        alt="profile"
-                      ></img>
+          <div className="diary_detail_prev_diary">
+            <button className="diary_detail_prev_btn">
+              <img src={process.env.PUBLIC_URL + `/img/prev.png`} />
+            </button>
+          </div>
+          <div className="diary_detail_post">
+            {dummy_diary
+              .filter((x) => x.created_at.slice(0, 10) === date)
+              .map((it) => (
+                <div>
+                  <div className="diary_detail_top">
+                    <div className="diary_detail_date">
+                      <p className="diary_detail_post_date">
+                        {it.created_at.slice(0, 10)}
+                      </p>
                     </div>
-                    <div className="diary_detail_author_post_info">
-                      <div className="diary_detail_author">
-                        <p> {it.user}</p>
-                      </div>
-                      <div className="diary_detail_post_detail">
-                        <div className="diary_detail_post_edit">
-                          <button>수정하기</button>
-                          <button>삭제하기</button>
-                        </div>
+                    <div className="diary_detail_post_title">{it.title}</div>
+
+                    <div className="diary_detail_post_detail">
+                      <div className="diary_detail_post_edit">
+                        <button>수정하기</button>
+                        <button>삭제하기</button>
                       </div>
                     </div>
                   </div>
+                  <div className="diary_detail_content_box">
+                    <div className="diary_detail_content">{it.content}</div>
+                  </div>
                 </div>
-                <div className="diary_detail_post_box">
-                  <div className="diary_detail_post">{it.post}</div>
-                </div>
-              </div>
-            ))}
+              ))}
+          </div>
+          <div className="diary_detail_next_diary">
+            <button className="diary_detail_next_btn">
+              <img src={process.env.PUBLIC_URL + `/img/next.png`} />
+            </button>
+          </div>
         </div>
       </div>
     </div>
