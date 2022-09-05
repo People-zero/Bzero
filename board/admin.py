7c884a93bd317b3579788a982841e04bce09d2f2
minseokey <lms990427@gmail.com>
2022-09-05 11:18:21 +0900

    [FIX] login required

--- board/admin.py
+++ board/admin.py
@@ -12,3 +12,6 @@ class commentAdmin(admin.ModelAdmin):
 
 
 
+
+
+
--- board/serializer.py
+++ board/serializer.py
@@ -1,9 +1,15 @@
 from rest_framework import serializers
 from . import models
+from accounts.models import User
 
 class PostSerializer(serializers.ModelSerializer):
     created_at = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S")
     updated_at = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S")
+    author_name = serializers.SerializerMethodField()
+
+    def get_author_name(self,request):
+        author_name = User.objects.get(id = request.author.id).username
+        return author_name
     
     class Meta:
         model = models.Post
@@ -14,7 +20,7 @@ class CommentSerializer(serializers.ModelSerializer):
     updated_at = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S")
     class Meta:
         model = models.Comment
-        fields = ("post","created_at","updated_at","content","author")
+        fields = ["post","created_at","updated_at","content","author","id",]
 
 
 class Post_DetailSerializer(serializers.ModelSerializer):
@@ -22,8 +28,11 @@ class Post_DetailSerializer(serializers.ModelSerializer):
     created_at = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S")
     updated_at = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S")
     
+
     class Meta:
         model = models.Post
-        fields = ("id",'comment_set',"created_at", "updated_at","category","content","image","recommend_user_set")
+        fields = ["id",'comment_set',"created_at", "updated_at","category","content","image","recommend_user_set"]
+        read_only_fields = ('comment_set',)
+
 
 
