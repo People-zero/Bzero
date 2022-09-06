b9ded62e02407de1441cb2c1eec0a3c9543e6e94
minseokey <lms990427@gmail.com>
2022-09-06 22:22:46 +0900

    [FIX] chayeong

--- board/serializer.py
+++ board/serializer.py
@@ -3,9 +3,7 @@ from . import models
 from accounts.models import User
 
 class PostSerializer(serializers.ModelSerializer):
-   
     author_name = serializers.SerializerMethodField()
-
     def get_author_name(self,request):
         author_name = User.objects.get(id = request.author.id).username
         return author_name
@@ -13,14 +11,18 @@ class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Post
         fields = "__all__"
-        read_only_fields = ("created_at","updated_at","author","recommend_user_set",)
+        read_only_fields = ("created_at","updated_at","recommend_user_set",)
 
 class CommentSerializer(serializers.ModelSerializer):
+
+    def get_author_name(self,request):
+        author_name = User.objects.get(id = request.author.id).username
+        return author_name
 
     class Meta:
         model = models.Comment
         fields = "__all__"
-        read_only_fields = ("created_at","updated_at","author","post")
+        read_only_fields = ("created_at","updated_at","author",)
 
 
 class Post_DetailSerializer(serializers.ModelSerializer):
